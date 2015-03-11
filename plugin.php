b3b2a19e9476a20ba3584eaa02e5fd60b6669fe6
James Golovich <james@gnuinter.net>
2015-03-11 01:38:56 -0700

    Move require up to make lib available earlier

--- plugin.php
+++ plugin.php
@@ -6,11 +6,14 @@ Author Name: James Golovich
 License: GPL2
 */
 
-add_action( 'init', 'pritect_exampleloader_withcomposer_startup' );
+add_action( 'plugins_loaded', 'pritect_exampleloader_withcomposer_startup' );
+add_action( 'init', 'pritect_exampleloader_withcomposer_go' );
 
 function pritect_exampleloader_withcomposer_startup() {
 	require 'vendor/autoload.php';
+}
 
+function pritect_exampleloader_withcomposer_go() {
 	$PAK = pritect_example_loader( array(), '1.0' );
 	$PAKnewest = pritect_example_loader();
 	$a = 1;
